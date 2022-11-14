ebdacb679fe504232582c0a89a43aeba620cf740
dingxt <dingxt@fosun.com>
2022-11-14 16:43:44 +0800

    redis单点分布式锁

---
+++ easypush/core/lock/__init__.py
@@ -0,0 +1,71 @@
+try:
+    from redlock import Redlock
+except ImportError:
+    Redlock = None
+
+import time
+import string
+import random
+import traceback
+
+from django_redis import get_redis_connection
+
+
+class AcquireLockError(Exception):
+    pass
+
+
+class ReleaseLockError(Exception):
+    pass
+
+
+class TaskRunningLockError(Exception):
+    pass
+
+
+def run_task_with_lock(lock_key,
+                       task, task_args=(), task_kwargs=None,
+                       task_pre=None, task_pre_args=(), task_pre_kwargs=None,
+                       expire=10 * 60, delay=0.1):
+    unlock_script = """
+        if redis.call("get",KEYS[1]) == ARGV[1] then
+            return redis.call("del",KEYS[1])
+        else
+            return 0
+        end
+    """
+    timestamp = int(time.time())
+    redis_conn = get_redis_connection()
+    uniq_val = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(22)).encode()
+
+    # Here is Distributed Lock
+    # 1: Self-implemented lock(distributed lock) for a single redis instance, redis.set is atomic,
+    #      but not blocking, so use `while` for checking, then must sleep
+    # 2: The redlock-py package is a distributed lock on multiple instances of redis，
+    #      after the lock is acquired, it is not blocking, you still need to use `while` for checking
+    while int(time.time()) - timestamp < expire:
+        if callable(task_pre):
+            pre_result = task_pre(*task_pre_args, **(task_pre_kwargs or {}))
+            if pre_result is not None:
+                return pre_result
+
+        # If the `task_run` business fails to be handle within 10 minutes or exception, the lock does not work
+        if redis_conn.set(lock_key, uniq_val, ex=expire, nx=True):
+            try:
+                if callable(task):
+                    return task(*task_args, **(task_kwargs or {}))
+            except Exception as e:
+                raise TaskRunningLockError("Task running business error: {0}".format(e))
+            finally:
+                # Recommend to use, only release the lock you put on yourself
+                try:
+                    redis_conn.eval(unlock_script, 1, lock_key, uniq_val)
+                except Exception as e:
+                    raise ReleaseLockError("Release lock error: {0}".format(e))
+
+                # Deprecated: The lock may not release itself
+                # conn.expire(lock_key, 0)  # Delete lock to success or fail
+
+        time.sleep(delay)  # Sleep 10 milliseconds
+
+    raise AcquireLockError("Acquire lock is failed with single redis instance")
--- easypush/utils/decorators.py
+++ easypush/utils/decorators.py
@@ -1,5 +1,10 @@
+from functools import wraps
+from datetime import datetime
+
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+
+token_cache = {}
 
 
 def exempt_view_csrf(view_cls):
@@ -7,4 +12,40 @@ def exempt_view_csrf(view_cls):
         return method_decorator(csrf_exempt, name="dispatch")(view_cls)
 
     return deco()
+
+
+def token_expire_cache(name, timeout=None, maxsize=128):
+    global token_cache
+    default_expire = 2 * 60 * 60
+
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            timestamp = datetime.now().timestamp()
+
+            # Clear expired tokens
+            for key in token_cache.keys():
+                token = token_cache[key]
+                expire = token.get("timeout", default_expire)
+                token_timestamp = token.get("timestamp", 0)
+
+                if timestamp - token_timestamp > expire:
+                    del token_cache[key]
+
+            token = token_cache.get(name, {})
+
+            if token:
+                return token["ret"]
+            else:
+                if len(token_cache) > maxsize:
+                    raise ValueError("Exceed maximum capacity:%s" % maxsize)
+
+                token_ret = func(*args, **kwargs)
+                token.update(timeout=timeout or default_expire, timestamp=timestamp, ret=token_ret)
+                token_cache[name] = token
+
+                return token_ret
+
+        return inner
+    return wrapper
 
